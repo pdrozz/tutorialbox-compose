eb01a7ed9f2b3033c19dae123661f47e27619d9a
Pdrozz <pedrohhcardoso@gmail.com>
2023-01-29 09:50:50 -0300

    chore: adds tutorialbox state

---
+++ TutorialBox/src/main/java/com/pdrozz/tutorialbox/state/TutorialBoxState.kt
@@ -0,0 +1,40 @@
+package com.pdrozz.tutorialbox.state
+
+import androidx.compose.foundation.layout.BoxScope
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateMapOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.layout.LayoutCoordinates
+
+class TutorialBoxState internal constructor(
+    initialIndex: Int,
+) {
+
+    internal var tutorialTargets = mutableStateMapOf<Int, TutorialBoxTarget>()
+
+    var currentTargetIndex by mutableStateOf(initialIndex)
+        internal set
+
+    val currentTarget: TutorialBoxTarget?
+        get() = tutorialTargets[currentTargetIndex]
+}
+
+data class TutorialBoxTarget(
+    val index: Int,
+    val coordinates: LayoutCoordinates,
+    val content: @Composable BoxScope.() -> Unit
+)
+
+@Composable
+fun rememberTutorialBoxState(
+    initialIndex: Int = 0,
+): TutorialBoxState {
+    return remember {
+        TutorialBoxState(
+            initialIndex = initialIndex,
+        )
+    }
+}
